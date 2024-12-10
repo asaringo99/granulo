79e1f1f353742d95ec3f5d8c6bb7af2f9e8af75f
asaringo <asaringo99@gmail.com>
2024-12-11 03:59:15 +0900

    wasmを利用できるようにする

--- granulo-ui/src/App.svelte
+++ granulo-ui/src/App.svelte
@@ -2,6 +2,7 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import Add from './lib/Add.svelte';
 </script>
 
 <main>
@@ -17,6 +18,9 @@
 
   <div class="card">
     <Counter />
+  </div>
+  <div>
+    <Add />
   </div>
 
   <p>
---
+++ granulo-ui/src/lib/Add.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { add } from "../wasm/granulo_mps";
+</script>
+
+<div>
+    {add(6, 4)}
+</div>
