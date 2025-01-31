aa14e9f5637fa80ad7b957837ddd46a83179e53c
asaringo <asaringo99@gmail.com>
2025-02-01 07:44:16 +0900

    style: headerをz-90にした

--- granulo-ui/src/component/header/Header.svelte
+++ granulo-ui/src/component/header/Header.svelte
@@ -4,7 +4,7 @@
 	let { routes } = $props();
 </script>
 
-<div class="w-full h-28 flex justify-around items-center fixed p-12 bg-slate-300 z-50">
+<div class="w-full h-28 flex justify-around items-center fixed p-12 bg-slate-300 z-[90]">
 	{#each routes as route}
 		<div class="duration-200 transition-transform">
 			<Link
